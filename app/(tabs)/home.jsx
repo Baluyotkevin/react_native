76ca276c230b74462f1fab58050e2d335cf750a2
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-05-29 13:35:20 -0700

    started videocard but video not playing >:(

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,78 +1,81 @@
-import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
-import SearchInput from '../../components/SearchInput'
-import Trending from '../../components/Trending'
-import Skeleton from '../../components/Skeleton'
-import { useEffect, useState } from 'react'
-import { getAllPosts } from '../../lib/appwrite'
-import useAppwrite from '../../lib/useAppwrite'
-import VideoCard from '../../components/VideoCard'
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
 
+import { images } from "../../constants";
+import useAppwrite from "../../lib/useAppwrite";
+import { getAllPosts, getTrendingPosts } from "../../lib/appwrite";
+import { Skeleton, SearchInput, Trending, VideoCard } from "../../components";
 
 const Home = () => {
-
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: trendingPosts } = useAppwrite(getTrendingPosts);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const [refreshing, setRefreshing] = useState(false)
- 
   const onRefresh = async () => {
     setRefreshing(true);
     await refetch();
     setRefreshing(false);
-  }
+  };
 
   return (
     <SafeAreaView className="bg-primary">
-      <FlatList 
+      <FlatList
         data={posts}
         keyExtractor={(item) => item.$id}
-        renderItem={({item}) => (
-          <VideoCard video={item} />
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
         )}
         ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
               <View>
-                <Text className='font-pmedium text-sm text-gray-100'>
+                <Text className="font-pmedium text-sm text-gray-100">
                   Welcome Back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                KB
+                  KB
                 </Text>
               </View>
-              <View className="mt-1 5">
-                <Image 
-                  src={images.logoSmall}
-                  className='w-9 h-10'
-                  resizeMode='contain'
+
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
                 />
               </View>
             </View>
 
             <SearchInput />
-            <View className='w-fuill flex-1 pt-5 pb-8'>
-              <Text className="text-gray-100 text-lg font-pregular mb-3">
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
                 Latest Videos
               </Text>
 
-              <Trending 
-                posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []}
-              />
+              <Trending posts={trendingPosts ?? []} />
             </View>
           </View>
         )}
-
         ListEmptyComponent={() => (
-          <Skeleton 
+          <Skeleton
             title="No Videos Found"
-            subtitle="Be the first one to ui"
+            subtitle="No videos created yet"
           />
         )}
-        refereshControl={<RefreshControl refrehing={refreshing} onRefresh={onRefresh} />}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ components/index.js
@@ -0,0 +1,15 @@
+import VideoCard from "./VideoCard";
+import FormField from "./FormField";
+import CustomButton from "./CustomButton";
+import Trending from "./Trending";
+import SearchInput from "./SearchInput";
+import Skeleton from "./Skeleton"
+
+export {
+  VideoCard,
+  FormField,
+  CustomButton,
+  Trending,
+  SearchInput,
+  Skeleton
+};
